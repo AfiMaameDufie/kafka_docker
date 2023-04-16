a99eece4535758174a445a3a4557dbe8653ce9a4
AfiMaameDufie <afimaamedufie@gmail.com>
2023-04-16 23:20:42 +0000

    Creating a producer for the topic

--- producer.py
+++ producer.py
@@ -3,5 +3,8 @@ from pykafka import KafkaClient
 # Create a Kafka Client
 client = KafkaClient(hosts="localhost:9092")
 
-#  Get the "sample" topic
+# Get the "sample" topic
 topic = client.topics['sample']
+
+# Create a producer
+producer = topic.get_sync_producer()
