5c789b3853370d85c314c3c433ba75425a541719
AfiMaameDufie <afimaamedufie@gmail.com>
2023-04-16 23:22:42 +0000

    Send a message to the topic Sample

--- producer.py
+++ producer.py
@@ -8,3 +8,6 @@ topic = client.topics['sample']
 
 # Create a producer
 producer = topic.get_sync_producer()
+
+# Send a message to the topic
+producer.produce(b"Hey! Sending this message as a test for Python Kafka.")
