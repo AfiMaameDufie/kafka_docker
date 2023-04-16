121207f89ed6e98c05d3111a30adfeade440b9b2
AfiMaameDufie <afimaamedufie@gmail.com>
2023-04-16 23:29:18 +0000

    Iterate through messages for sample topic

--- consumer.py
+++ consumer.py
@@ -6,3 +6,8 @@ topic = client.topics['sample']
 
 # Creating a consumer
 consumer = topic.get_simple_consumer()
+
+# Go through messages for the topic: sample
+for msg in consumer:
+    if msg is not None:
+        print(msg.offset, msg.value)
